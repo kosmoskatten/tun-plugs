f9af7de9693aa097b683232d0096c08a9b4504f4
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-06-11 16:30:50 +0200

    Parameterized the tun interface to use.

--- src/TunToNet.hs
+++ src/TunToNet.hs
@@ -5,13 +5,15 @@ module Main
 import Data.ByteString (ByteString)
 import Conduit
 import Data.Conduit.Network
+import System.Environment (getArgs)
 import System.IO.Tun (openTun)
 
 import qualified Data.ByteString as BS
 
 main :: IO ()
 main = do
-    mTunH <- openTun "tun0"
+    [tun] <- getArgs
+    mTunH <- openTun tun
     case mTunH of
         Just tunH -> sourceHandle tunH $$ sizeTeller
         Nothing   -> putStrLn "Cannot open tun device."
